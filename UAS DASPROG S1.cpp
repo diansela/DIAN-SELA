4752e6a0a859e6fc9296806fa56075b2b028bd15
diansela <122856246+diansela@users.noreply.github.com>
2023-01-27 23:17:15 +0700

    Update UAS DASPROG S1.cpp

--- UAS DASPROG S1.cpp
+++ UAS DASPROG S1.cpp
@@ -25,11 +25,11 @@ void aplikasieditorvideo() {
     cout << "================ \n";
 
     pilihan ftr [15];
-    ftr [0].inshot_cut 	= " 1. Pilih video yang akan diedit\n 2. Pilih fitur pra-potong\n 3. Pilih trim (untuk memtong diawal atau akhir video)\n";
-    ftr [0].inshot_cut2 = " 4. Pilih cut untuk memotong bagian tengah video\n 5. Pilih split (untuk membelah video)";
-	ftr [1].inshot_teks = " 1. Bukalah fitur teks di aplikasi inshot\n 2. Tulis kata-kata yang kamu inginkan dan ceklis jika selesai\n 3. Aturlah posisi teks agar sesuai dengan video\n";
+    	ftr [0].inshot_cut 	= " 1. Pilih video yang akan diedit\n 2. Pilih fitur pra-potong\n 3. Pilih trim (untuk memtong diawal atau akhir video)\n";
+    	ftr [0].inshot_cut2 	= " 4. Pilih cut untuk memotong bagian tengah video\n 5. Pilih split (untuk membelah video)";
+	ftr [1].inshot_teks 	= " 1. Bukalah fitur teks di aplikasi inshot\n 2. Tulis kata-kata yang kamu inginkan dan ceklis jika selesai\n 3. Aturlah posisi teks agar sesuai dengan video\n";
 	ftr [2].inshot_dpl 	= " 1. Pilih bagian video yang ingin di duplikat\n 2. Pilih fitur duplikat maka video yang kamu pilih akan tercopy";
-	ftr [3].ftr_keceptn = " 1. Bukalah fitur edit\n 2. Pilih fitur kecepatan\n 3. Lalu pilih ingin menggunakan kecepatan normal atau kecepatan kurva\n";
+	ftr [3].ftr_keceptn 	= " 1. Bukalah fitur edit\n 2. Pilih fitur kecepatan\n 3. Lalu pilih ingin menggunakan kecepatan normal atau kecepatan kurva\n";
 	ftr [4].ftr_audio 	= " 1. Bukalah fitur audio\n 2. Pilih fitur suara (untuk mengekstrak suara dari musik yang kamu inginkan)\n ";
 	ftr [4].ftr_audio2 	= "3. Pilih fitur efek (untuk menambahkan musik bawaan capcut)\n 4. Disana kamu bisa menambahkan efek suara\n";
 	ftr [4].ftr_audio3 	= " 5. Pilih fitur diekstrak (untuk mengekstrak suara dari video)\n 6. Pilih fitur rekam (untuk menambah rekaman suara)\n";
@@ -47,7 +47,6 @@ void aplikasieditorvideo() {
 				char ulang1;
 				do {
 				char pilih_fitur;
-
 					cout << "==========Aplikasi Inshot============" << endl;
 					cout << "a. Cara Mengcut Video \n";
 					cout << "b. Cara Menambahkan Teks \n";
@@ -58,36 +57,34 @@ void aplikasieditorvideo() {
 					cout << "================ \n";
 					switch (pilih_fitur){
 						case 'a':
-						    cout << "Cara Mengcut Video di Aplikasi Inshot\n";
-                			cout << ftr [0].inshot_cut;
-                			cout << ftr [0].inshot_cut2;
-						    break;
+						    	cout << "Cara Mengcut Video di Aplikasi Inshot\n";
+                					cout << ftr [0].inshot_cut;
+                					cout << ftr [0].inshot_cut2;
+						    	break;
 						case 'b':
-                            cout << "Cara Menambahkan Teks Pada Video di Aplikasi Inshot\n";
-                			cout << ftr [1].inshot_teks;
-						    break;
+                            				cout << "Cara Menambahkan Teks Pada Video di Aplikasi Inshot\n";
+                					cout << ftr [1].inshot_teks;
+						    	break;
 						case 'c':
-						    cout << "Cara Menduplikat Video di Aplikasi Inshot\n";
-                			cout << ftr [2].inshot_dpl;
-						    break;
+						    	cout << "Cara Menduplikat Video di Aplikasi Inshot\n";
+                					cout << ftr [2].inshot_dpl;
+						    	break;
 						default :
-						    cout << "tidak ada fitur yang dipilih\n";
-							}
-							cout << endl;
-							cout << "pilih fitur lain? (y/t) : ";
-							cin >> ulang1;
-							cout << endl;
-							}
-							while (ulang1 != 't');
-							cout << "akhir dari program\n";
+						    	cout << "tidak ada fitur yang dipilih\n";
+					}
+						cout << endl;
+						cout << "pilih fitur lain? (y/t) : ";
+						cin >> ulang1;
+						cout << endl;
+				}while (ulang1 != 't');
+						cout << "akhir dari program\n";
     						break;
     	case 2:
     		system ("cls");
 				char ulang2;
 				do {
 				char pilih_vct;
-
-                    cout << "==========Aplikasi CapCut============\n";
+                    			cout << "==========Aplikasi CapCut============\n";
 					cout << "a. Cara Mengatur Kecepatan Video \n";
 					cout << "b. Menambah Efek Suara \n";
 					cout << "c. Efek Filter \n";
@@ -97,35 +94,33 @@ void aplikasieditorvideo() {
 					cout << "================ \n";
 					switch (pilih_vct){
 						case 'a':
-						    cout << " cara mengatur kecepatan video di aplikasi capcut : \n";
-						    cout << ftr [3].ftr_keceptn;
-						    break;
+						    	cout << " cara mengatur kecepatan video di aplikasi capcut : \n";
+						    	cout << ftr [3].ftr_keceptn;
+						    	break;
 						case 'b':
-						    cout << "untuk menambah efek suara pada aplikasi capcut : \n";
-						    cout << ftr [4].ftr_audio;
-                			cout << ftr [4].ftr_audio2;
-                			cout << ftr [4].ftr_audio3;
-						    break;
+						    	cout << "untuk menambah efek suara pada aplikasi capcut : \n";
+						    	cout << ftr [4].ftr_audio;
+                					cout << ftr [4].ftr_audio2;
+                					cout << ftr [4].ftr_audio3;
+							break;
 						case 'c':
-                            cout << "cara menambahkan efek filter di aplikasi capcut : \n";
-						    cout << ftr [5].ftr_filtr;
-						    break;
+                           	 			cout << "cara menambahkan efek filter di aplikasi capcut : \n";
+						    	cout << ftr [5].ftr_filtr;
+						    	break;
 						default :
-						    cout << "tidak ada fitur yang dipilih\n";
-							}
-							cout << endl;
-							cout << "pilih fitur lain? (y/t) : ";
-							cin >> ulang2;
-							}
-							while (ulang2 != 't');
-							cout << "akhir program\n";
-    		break;
+						    	cout << "tidak ada fitur yang dipilih\n";
+					}
+						cout << endl;
+						cout << "pilih fitur lain? (y/t) : ";
+						cin >> ulang2;
+				}while (ulang2 != 't');
+						cout << "akhir program\n";
+    						break;
     	case 3:
     		system ("cls");
 				char ulang3;
 				do {
 				char pilih_fictur;
-
 					cout << "==========Aplikasi VlogNow (VN)============" << endl;
 					cout << "a. Cara Mengatur Volume Suara Video \n";
 					cout << "b. Cara Mengubah Ukuran Asli Video \n";
@@ -137,21 +132,21 @@ void aplikasieditorvideo() {
 					switch (pilih_fictur){
 						case 'a':
 							cout << "Cara Mengubah Volume Suara Video di Aplikasi VN\n";
-						    cout << ftr [6].vn_vlm;
-						    cout << ftr [6].vn_vlm2;
-						    break;
+						    	cout << ftr [6].vn_vlm;
+						    	cout << ftr [6].vn_vlm2;
+						    	break;
 						case 'b':
-						    cout << "Cara Mengukur Ukuran Asli Video Menjadi Ukuran Lain\n";
-						    cout << ftr [7].vn_ptng;
-						    cout << ftr [7].vn_ptng2;
-						    break;
+						    	cout << "Cara Mengukur Ukuran Asli Video Menjadi Ukuran Lain\n";
+						    	cout << ftr [7].vn_ptng;
+						    	cout << ftr [7].vn_ptng2;
+						    	break;
 						case 'c':
-						    cout << "Cara Mengahapus Video di Aplikasi VN \n";
-						    cout << ftr [8].vn_hps;
-						    cout << ftr [8].vn_hps2;
+						    	cout << "Cara Mengahapus Video di Aplikasi VN \n";
+						    	cout << ftr [8].vn_hps;
+						    	cout << ftr [8].vn_hps2;
 							break;
 						default :
-						    cout << "tida ada fitur yang dipilih\n";
+						    	cout << "tida ada fitur yang dipilih\n";
 							}
 							cout << endl;
 							cout << "pilih fitur lain? (y/t) : ";
@@ -163,11 +158,11 @@ void aplikasieditorvideo() {
     	default :
     		cout << "tidak ada pilihan apk\n";
 	}
-    cout << endl;
+    	cout << endl;
 	cout << "pilih ulang program aplikasi editor? (y/t) : ";
 	cin >> ulang;
 	cout << endl;
-    system ("cls");
+    	system ("cls");
 	} while (ulang != 't');
 	cout << endl;
 	cout << "\t#program selesai dijalankan#\n";
